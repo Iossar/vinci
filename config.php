21c73824e8a081a5c6c344577c354ac96f63a8e8
Iossar <kleymenov@save24.ru>
2020-07-08 09:15:48 +0300

    change composer.json for namespace autoloading

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 defined("DBDRIVER")or define("DBDRIVER","mysql");
 defined("DBHOST")or define("DBHOST","localhost");
 defined("DBNAME")or define("DBNAME","vinci");
-defined("DBUSER")or define("DBUSER","nick");
-defined("DBPASS")or define("DBPASS","2468");
+defined("DBUSER")or define("DBUSER","");
+defined("DBPASS")or define("DBPASS","");
--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 
 require_once 'vendor/autoload.php';
 require_once 'config.php';
-require_once 'app/controllers/GitController.php';
-require_once 'app/controllers/UserController.php';
-require_once 'app/models/User.php';
-require_once 'app/models/Database.php';
 
 use app\controllers\GitController;
 use app\controllers\UserController;
